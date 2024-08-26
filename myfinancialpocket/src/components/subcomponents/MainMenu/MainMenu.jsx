8ad6b60a73f7166b9322ccc46a8bfb18a9235949
Jose Luis <condegomesjoseluis@gmail.com>
2024-08-26 17:16:35 +0200

    Feat(Header): create menu subcomponent for header

---
+++ myfinancialpocket/src/components/subcomponents/MainMenu/MainMenu.jsx
@@ -0,0 +1,18 @@
+import './MainMenu.css';
+
+function MainMenu() {
+    
+
+    return (
+        <nav>
+            <ul>
+                <li>INICIO</li>
+                <li>TRANSACCIONES</li>
+                <li>PRESUPUESTOS</li>
+                <li>ANÁLISIS</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default MainMenu;
